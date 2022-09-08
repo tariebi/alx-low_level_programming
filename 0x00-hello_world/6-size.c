f23fdc9c302b9dbe636b1d310964b3d484e86458
tariebi <tariebi.agedah@stu.cu.edu.ng>
2022-09-08 16:40:04 -0700

    'task 6'

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,13 +8,11 @@
  * Return: 0
  */
 int main(void)
-{
-       	char c;
+{	char c;
        	int i;
 	long li; 
 	long long lli; 
 	float f;
-
 	printf("size of a char: %ld byte(s)\n", sizeof(c));
 	printf("size of an int: %ld byte(s)\n", sizeof(i));
 	printf("size of a long int: %ld byte(s)\n", sizeof(li));
