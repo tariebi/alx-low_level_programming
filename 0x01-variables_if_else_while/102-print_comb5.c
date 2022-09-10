32b3348090eff075389ab0495e5eab85b7d73f9e
tariebi <tariebi.agedah@stu.cu.edu.ng>
2022-09-10 16:11:58 -0700

    'task 12'

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,97 +3,60 @@
 
 /**
  *
+ *  * main - prints all possible combinations of two two-digit numbers
  *
- *  * main - print possible combination of two 2-digit
+ *   * Return: Always 0 (Success)
  *
- *
- *   * numbers
- *
- *
- *    *
- *
- *
- *     * Return: returns zero at the end
- *
- *
- *      */
-
+ *    */
 
 int main(void)
 
-
 {
 
-
-	    int i, p;
-
+	  int i, j;
 
 
+	    for (i = 0; i < 100; i++)
+
+		      {
+
+			          for (j = 0; j < 100; j++)
+
+					      {
+
+						            if (i < j)
+
+								          {
+
+										          putchar((i / 10) + 48);
+
+											          putchar((i % 10) + 48);
+
+												          putchar(' ');
+
+													          putchar((j / 10) + 48);
+
+														          putchar((j % 10) + 48);
+
+															          if (i != 98 || j != 99)
+
+																	          {
+
+																			            putchar(',');
+
+																				              putchar(' ');
+
+																					              }
+
+																        }
 
 
+							        }
 
+				    }
 
-	        for (i = 0; i < 100; i++)
+	      putchar('\n');
 
-
-			    {
-
-
-				            for (p = 0; p < 100; j++)
-
-
-						            {
-
-
-								                if (p > i)
-
-
-											            {
-
-
-													                    putchar((i / 10) + '0');
-
-
-															                    putchar((i % 10) + '0');
-
-
-																	                    putchar(' ');
-
-
-																			                    putchar((p / 10) + '0');
-
-
-																					                    putchar((p % 10) + '0');
-
-
-																							                    if (i != 98)
-
-
-																										                    {
-
-
-																													                        putchar(',');
-
-
-																																                    putchar(' ');
-
-
-																																		                    }
-
-
-																									                }
-
-
-										        }
-
-
-					        }
-
-
-		    putchar('\n');
-
-
-		        return (0);
-
+	        return (0);
 
 }
