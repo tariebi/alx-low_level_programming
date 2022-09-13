25281293b91aac4028464ad9935cda3146d80d55
tariebi <tariebi.agedah@stu.cu.edu.ng>
2022-09-13 16:37:32 -0700

    'task 3'

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *islower - function check if the character is lowercase
+ *_islower - function check if the character is lowercase
  *return: returns 1 and 0 depending on condition
  */
 int _islower(int c)
