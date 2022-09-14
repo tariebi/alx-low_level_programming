3bbf029d29d920191aa19dcc652e0f375be19514
tariebi <tariebi.agedah@stu.cu.edu.ng>
2022-09-13 17:19:11 -0700

    'task 5'

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 /**
  * _isalpha - function to check if c is a letter, lowercase or uppercase
  * @c: is the int that will use for the argument of the function
- * return : 0
+ * return : 0 or 1 depending on condition
  */
 int _isalpha(int c)
 {
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,7 +14,7 @@ int print_sign (int n)
 	else if (n < 0 )
 	{
 	_putchar ('-');
-	retyrn (-1);
+	return (-1);
 	}
 	else
 	{
