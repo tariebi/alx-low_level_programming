fcb9f3949f7decef1ed253a55e9ffc99a797e24c
tariebi <tariebi.agedah@stu.cu.edu.ng>
2022-09-16 12:56:47 -0700

    'task 0'

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#Include "main.h"
+
+/**
+ * main - tests function that prints if the integer is positive or negative
+ * return : 0
+ */
+
+int main(void)
+{
+int i;
+
+i = 0;
+positive_or_negative(i);
+
+return (0);
+}
