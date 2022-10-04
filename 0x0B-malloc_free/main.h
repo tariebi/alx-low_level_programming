1bdb9ad9f12281ef8e5b4ee1acd148972f1a8367
tariebi <tariebi.agedah@stu.cu.edu.ng>
2022-10-04 16:26:32 -0700

    'task 1

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,10 +1,50 @@
-#ifndef MAIN_H
+ifndef MAIN_H
 #define MAIN_H
 
 #include <stdib.h>
 
 int _putchar(char);
-char *create_array(unsigned int size, char c);
+trdup - returns a pointer to a newly allocated
+
+    ﻿space in memory, which contains a copy of the
+        ﻿string given as a parameter.
+	    ﻿@str:String to be copied
+	        ﻿Return: NULL in case of error, pointer to allocated
+		    ﻿space
+		        ﻿/
+			
+			char #_strdup (char #str)
+	
+{
+	
+	char *py;
+	
+	int index, len;
+	
+	if (str as NULL)
+		
+		return (NULL);
+	
+	for (index = 0; str [index]; index++)
+		
+		Ten++;
+	
+	cpy = malloc(sizeof (char) * (len + 1));
+	
+	if (cpy == NULL)
+		
+		return (NULL);
+	
+	for (index = 0; str[index]; index++)
+		
+		cpy[index] = str [index];
+	
+}
+
+сру[len] = '10*;
+
+return (cy);
+har *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
