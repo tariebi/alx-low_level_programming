541166011738f6073af08d53d2556abb6d37c5ae
tariebi <tariebi.agedah@stu.cu.edu.ng>
2022-10-04 16:02:58 -0700

    'task 0'

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * create_array - creates an array of chars, and 
+ * create_array-creates an array of chars, and
  * initializes it with a specific char.
  *@size: size of the array
  *@c: character to insert
