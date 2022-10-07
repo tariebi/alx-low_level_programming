db5dfda5d180388fd707c22d7eef1785985fcfe7
tariebi <tariebi.agedah@stu.cu.edu.ng>
2022-10-06 19:19:09 -0700

    task

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,12 +10,12 @@
  */
 int *array_range(int min, int max)
 {
-	int i, 1;
+	int i, l;
 	int *a;
 
 	if (min > max)
 		return (NULL);
-	1 = max - min + 1;
+	l = max - min + 1;
 	a = malloc(sizeof(int) * 1);
 	if (a == NULL)
 		return (NULL);
